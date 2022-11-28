61b8bef10870ccc4dc0096f1b421675526a0e4da
manuel101284 <manuel101284@gmail.com>
2022-11-28 18:41:50 -0500

    creación del repositorio

---
+++ js/wheelOfDoom.js
@@ -0,0 +1,36 @@
+var sacrificio = [];
+
+document.onload = function(){
+    document.sacrificio = [];
+}
+function agregarAlma(){
+    var nuevaalma = document.getElementById('alma').value;
+    var longitud = sacrificio.length;
+    var contador=0;
+
+    for(let i=0; i<longitud; i++){
+        if(sacrificio[i] === nuevaalma){
+            contador = contador + 1;    
+        }
+    }
+    if(contador==0){
+        sacrificio.push(nuevaalma);
+    }
+    else{
+        alert("Esta alma ya existe");
+    }
+
+    
+}
+
+function mostrarAlma(){
+    var resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+
+    for(const nuevaalma of sacrificio){
+        var datoParrafo = document.createElement('p');
+        datoParrafo.innerText = nuevaalma;
+
+        resultado.appendChild(datoParrafo);
+    }
+}
